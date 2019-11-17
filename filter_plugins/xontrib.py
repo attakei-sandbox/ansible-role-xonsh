7f165726600fb3ede1a62ce13eb052d937c749fc
Kazuya Takei <attakei@gmail.com>
2019-11-17 17:24:13 +0900

    Installation xontrib

--- filter_plugins/xontrib.py
+++ filter_plugins/xontrib.py
@@ -6,42 +6,37 @@ from ansible import errors  # noqa
 ansi_escape_8bit = re.compile(br'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]')
 
 
-class Xontrib(object):
-    def __init__(self, name: str, installed: bool, loaded: bool):
-        self.name = name
-        self.installed = installed
-        self.loaded = loaded
-
-    def __repr__(self):
-        msg = f'Xontrib: {self.name}'
-        if self.loaded:
-            msg += ' (loaded)'
-        elif self.installed:
-            msg += ' (installed)'
-        else:
-            msg += ' (not installed)'
-        return msg
-
-    @classmethod
-    def parse_from_shell(cls, line: str):
-        parts = [
-            ansi_escape_8bit.sub(b'', p.encode()).decode()
-            for p in line.split()
-        ]
-        print(parts)
-        return cls(parts[0], parts[1] == 'installed', parts[2] == 'loaded')
+def parse_from_shell(line: str):
+    parts = [
+        ansi_escape_8bit.sub(b'', p.encode()).decode()
+        for p in line.split()
+    ]
+    return {
+        'name': parts[0],
+        'installed': parts[1] == 'installed',
+        'loaded': parts[2] == 'loaded',
+    }
 
 
-def parse_xontribs(lines: List[str]) -> Dict[str, Xontrib]:
+def parse_xontribs(lines: List[str]) -> Dict[str, Dict]:
     xontribs = {}
     for l in lines:
-        xontrib = Xontrib.parse_from_shell(l)
-        xontribs.setdefault(xontrib.name, xontrib)
+        xontrib = parse_from_shell(l)
+        xontribs.setdefault(xontrib['name'], xontrib)
     return xontribs
+
+
+def xontrib_not_installed(xontribs: List[Dict]) -> List[str]:
+    return [
+        xontrib['name']
+        for xontrib in xontribs.values()
+        if not xontrib['installed']
+    ]
 
 
 class FilterModule(object):
     def filters(self):
         return {
             'parse_xontribs': parse_xontribs,
+            'xontrib_not_installed': xontrib_not_installed,
         }
